512b40bd652b3bd8eca5824d708654cf5fc0519f
viccoll <70841204+viccoll@users.noreply.github.com>
2020-09-29 13:30:36 +0300

    feat: min,max,avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+exports.min = function min(array) {
+    if (arguments.length === 0 || array.length === 0) return 0;
+    else {
+        let min = array[0];
 
-exports.min = function min (array) {
-  return 0;
-}
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < min) min = array[i];
+        }
+        return min;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (arguments.length === 0 || array.length === 0) return 0;
+    else {
+        let max = array[0];
 
-exports.avg = function avg (array) {
-  return 0;
-}
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > max) max = array[i];
+        }
+        return max;
+    }
+};
+
+exports.avg = function avg(array) {
+    if (arguments.length === 0 || array.length === 0) return 0;
+    else {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    }
+};
